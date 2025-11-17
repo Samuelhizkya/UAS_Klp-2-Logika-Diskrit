490db483b5fcb28a7c499829c8c0d65fe4951cb3
Samuelhizkya <semyulhizkya@gmail.com>
2025-11-17 23:51:06 +0800

    Implement interactive boolean logic for triage
    Added interactive boolean logic module for triage.

--- Aljabar Boolean_Vivi.py
+++ Aljabar Boolean_Vivi.py
@@ -1,1 +1,22 @@
+# -------------------------
+# MODUL 5 : ALJABAR BOOLEAN & LOGIKA DIGITAL
+# -------------------------
+def modul_boolean_interaktif():
+    try:
+        cetak_box("MODUL 5 : ALJABAR BOOLEAN", [
+            "Masukkan kondisi (True/False)"
+        ])
+        darurat = input("Darurat? (True/False): ").strip().title() == "True"
+        ada_kamar = input("Ada kamar? (True/False): ").strip().title() == "True"
+
+        diterima = (darurat and ada_kamar) or (darurat and not ada_kamar)
+
+        cetak_box("Hasil Triase Sederhana", [
+            f"Status darurat: {darurat}",
+            f"Ada kamar: {ada_kamar}",
+            f"Apakah pasien diterima?: {diterima}"
+        ])
+    except Exception as e:
+        print("Terjadi kesalahan di modul boolean:", e)
+
 
